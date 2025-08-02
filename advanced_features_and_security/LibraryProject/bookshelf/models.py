a10abea968ba03ac285fd3b4e2db1295e91b71fe
Thaddeus Onyeakazi <tonyeakazi23@gmail.com>
2025-08-02 16:05:37 +0100

    feat: Corrected project layout to a single root directory

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++
@@ -1,4 +0,0 @@
-# LibraryProject/bookshelf/models.py
-
-# This file is intentionally left empty for checker compatibility for this task.
-# Models for the bookshelf app will be defined here in a future task.
