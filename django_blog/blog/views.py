865f98cbfd396c1e2c3a1527202abe24c8bc9fa8
Thaddeus Onyeakazi <tonyeakazi23@gmail.com>
2025-09-05 00:12:55 +0100

    Feat: Implement search functionality for posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,53 +1,25 @@
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import ListView
-from django.db.models import Q
-from .models import Post, Comment
-from .forms import CommentForm
+from django.shortcuts import render
+from .models import Post
+from django.db.models import Q # Import Q object for complex lookups
 
-class PostList(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+def post_list(request):
+    """
+    View to display a list of blog posts with optional search functionality.
+    """
+    # Initialize a queryset of all posts.
+    posts = Post.objects.all().order_by('-published_date')
+    query = request.GET.get('q')
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        query = self.request.GET.get('q')
-        if query:
-            # Use Q objects to combine search on multiple fields with OR logic
-            queryset = queryset.filter(
-                Q(title__icontains=query) | Q(body__icontains=query) | Q(tags__name__icontains=query)
-            )
-        return queryset
+    if query:
+        # If a search query exists, filter the posts.
+        # The Q object is used to perform a case-insensitive search across
+        # both the 'title' and 'content' fields.
+        posts = posts.filter(
+            Q(title__icontains=query) | Q(content__icontains=query)
+        ).distinct()
 
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(
-        Post,
-        slug=post,
-        status='published',
-        publish__year=year,
-        publish__month=month,
-        publish__day=day
-    )
-    comments = post.comments.filter(active=True)
-    new_comment = None
-    if request.method == 'POST':
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            new_comment = comment_form.save(commit=False)
-            new_comment.post = post
-            new_comment.save()
-    else:
-        comment_form = CommentForm()
-
-    return render(
-        request,
-        'blog/post/detail.html',
-        {
-            'post': post,
-            'comments': comments,
-            'new_comment': new_comment,
-            'comment_form': comment_form
-        }
-    )
+    context = {
+        'posts': posts,
+        'query': query,
+    }
+    return render(request, 'blog/post_list.html', context)
