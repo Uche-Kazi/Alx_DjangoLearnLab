20cb662c5445c197e740d43ee223749446bdac15
Thaddeus Onyeakazi <tonyeakazi23@gmail.com>
2025-09-04 20:17:07 +0100

    Fix: Add TagWidget to PostForm to enable tagging functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,17 +1,27 @@
 from django import forms
-from .models import Comment
+
+# Import the new widget for handling tags
+from tag_manager.widgets import TagWidget
+from .models import Post, Comment
+
+class PostForm(forms.ModelForm):
+    """
+    Form for creating and updating blog posts.
+    """
+    class Meta:
+        model = Post
+        fields = ('title', 'slug', 'author', 'content', 'status', 'tags')
+        
+        # Configure the widgets to use the custom TagWidget for the 'tags' field.
+        # This tells the form to render the tags using our custom-built widget.
+        widgets = {
+            'tags': TagWidget(),
+        }
 
 class CommentForm(forms.ModelForm):
     """
-    A form for users to submit comments on blog posts.
-    It uses the Comment model and only includes the 'name', 'email', and 'body' fields.
+    Form for creating comments on blog posts.
     """
     class Meta:
         model = Comment
-        fields = ('name', 'email', 'body')
-
-class PostSearchForm(forms.Form):
-    """
-    A simple form for searching blog posts by keywords.
-    """
-    query = forms.CharField()
+        fields = ('name', 'email', 'content')
