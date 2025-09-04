c762d5c53b25febaa6723a875dc1deeec89f4ad6
Thaddeus Onyeakazi <tonyeakazi23@gmail.com>
2025-09-04 20:23:50 +0100

    Refactor: Update PostList to include search query filter

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,98 +1,43 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.db.models import Count
+from django.views import generic
 from .models import Post, Comment
-from .forms import CommentForm, PostSearchForm
-from taggit.models import Tag
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from .forms import CommentForm
 from django.db.models import Q
 
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 3
 
-def post_list(request, tag_slug=None):
-    """
-    View to list all published blog posts, with optional filtering by tag.
-    """
-    object_list = Post.published.all()
-    tag = None
+    def get_queryset(self):
+        # Start with the base queryset of published posts.
+        queryset = super().get_queryset()
 
-    if tag_slug:
-        tag = get_object_or_404(Tag, slug=tag_slug)
-        object_list = object_list.filter(tags__in=[tag])
+        # Check for a search query.
+        if 'q' in self.request.GET:
+            # Get the search term and apply filters to both content and tags.
+            q = self.request.GET['q']
+            queryset = queryset.filter(
+                Q(content__icontains=q) | Q(tags__name__icontains=q)
+            ).distinct()
+        
+        return queryset
 
-    paginator = Paginator(object_list, 3) # 3 posts per page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
-    return render(request,
-                  'blog/post/list.html',
-                  {'page': page,
-                   'posts': posts,
-                   'tag': tag})
-
-def post_detail(request, year, month, day, post):
-    """
-    View to display a single post, its comments, and a comment form.
-    """
-    # Retrieve the post object or return a 404 if not found
-    post = get_object_or_404(Post, slug=post,
-                                   status='published',
-                                   publish__year=year,
-                                   publish__month=month,
-                                   publish__day=day)
-
-    # Get all active comments for this post
+def post_detail(request, slug):
+    template_name = 'post_detail.html'
+    post = get_object_or_404(Post, slug=slug)
     comments = post.comments.filter(active=True)
     new_comment = None
-
     if request.method == 'POST':
-        # A comment was posted
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
-            # Create Comment object but don't save to database yet
             new_comment = comment_form.save(commit=False)
-            # Assign the current post to the comment
             new_comment.post = post
-            # Save the comment to the database
             new_comment.save()
     else:
         comment_form = CommentForm()
 
-    # Retrieve a list of similar posts
-    post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
-
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post': post,
-                   'comments': comments,
-                   'new_comment': new_comment,
-                   'comment_form': comment_form,
-                   'similar_posts': similar_posts})
-
-def post_search(request):
-    """
-    View to handle post searches.
-    """
-    form = PostSearchForm()
-    query = None
-    results = []
-
-    if 'query' in request.GET:
-        form = PostSearchForm(request.GET)
-        if form.is_valid():
-            query = form.cleaned_data['query']
-            # Search using a simple OR query on title and body
-            results = Post.published.filter(Q(title__icontains=query) | Q(body__icontains=query))
-
-    return render(request,
-                  'blog/post/search.html',
-                  {'form': form,
-                   'query': query,
-                   'results': results})
+    return render(request, template_name, {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
